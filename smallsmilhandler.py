7458c07704c3a45429af9933839e3e5ac8612fab
cnadals <carmen.nadal@gmail.com>
2016-10-03 16:10:42 +0200

    smallsmilhandler.py startElem

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -7,34 +7,29 @@ from xml.sax.handler import ContentHandler
 class SmallSMILHandler(ContentHandler):
 
 	def __init__ (self):
-	"""
-	Constructor. Inicializamos las variables
-	"""
-	pass
+
+		pass
 
     def startElement(self, name, attrs):
-        """
-        Método que se llama cuando se abre una etiqueta
-        """
+
         if name == 'root-layout':
-            # De esta manera tomamos los valores de los atributos
-            self.width = attrs.get('root-layout',"")
-            self.height = attrs.get('root-layout',"")
-            self.backgroundcolor = attrs.get('root-layout',"")
+            self.width = attrs.get('width',"")
+            self.height = attrs.get('height',"")
+            self.backgroundcolor = attrs.get('backgroundcolor',"")
         elif name == 'region':
-            self.id = attrs.get('region',"")
-            self.top = attrs.get('region',"")
-            self.bottom = attrs.get('region',"")
-            self.left = attrs.get('region',"")
-            self.right = attrs.get('region',"")
+            self.id = attrs.get('id',"")
+            self.top = attrs.get('top',"")
+            self.bottom = attrs.get('bottom',"")
+            self.left = attrs.get('left',"")
+            self.right = attrs.get('right',"")
         elif name == 'img':
-            self.src = attrs.get('img',"")
-            self.region = attrs.get('img',"")
-            self.begin = attrs.get('img',"")
-            self.dur = attrs.get('img',"")
+            self.src = attrs.get('src',"")
+            self.region = attrs.get('region',"")
+            self.begin = attrs.get('begin',"")
+            self.dur = attrs.get('dur',"")
         elif name == 'audio':
-        	self.begin = attrs.get('audio',"")
-        	self.dur = attrs.get('audio',"")
+        	self.begin = attrs.get('begin',"")
+        	self.dur = attrs.get('dur',"")
         elif name == 'textstream':
-        	self.src = attrs.get('textstream',"")
-        	self.region = attrs.get('textstream',"")
+        	self.src = attrs.get('src',"")
+        	self.region = attrs.get('region',"")
